cb2b291b36e05436950ed68d125cee71c486f4d8
sam-wolfe <samwolfe37@gmail.com>
2023-02-05 09:09:04 -0800

    UI, post processing, scream trigger

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -1,14 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Frog : MonoBehaviour {
+    
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _devScream;
+    [SerializeField] AudioClip _scream2;
+    [SerializeField] AudioClip _scream3;
+    [SerializeField] private Animator _animator;
+    private List<Tuple<String, AudioClip>> _screams = new List<Tuple<string, AudioClip>>();
+    
+    bool _isScreaming = false;
 
-    void Start() {
-        
+    private void Start() {
+        _screams.Add(new Tuple<string, AudioClip>("Scream1", _devScream));
+        _screams.Add(new Tuple<string, AudioClip>("Scream2", _scream2));
+        _screams.Add(new Tuple<string, AudioClip>("Scream3", _scream3));
     }
 
     void Update() {
         
+        // If player presses w actiavte animation trigger "Scream1", e for "Scream2", r for "Scream3"
+        if (Input.GetKeyDown(KeyCode.W)) {
+            _animator.SetTrigger("Scream1");
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            _animator.SetTrigger("Scream2");
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            _animator.SetTrigger("Scream3");
+        }
+        
+    }
+
+    public void FrogScream() {
+        // This method will be triggered by a UI button. 
+        // When run, trigger a random scream and play audio clip from the list.
+        // Run play random scream coroutine
+        StartCoroutine(PlayRandomScream());
+    }
+    
+    // coroutine to play random scream, wont run if already playing, resets bool when done
+    IEnumerator PlayRandomScream() {
+        if (!_isScreaming) {
+            _isScreaming = true;
+            int randomScream = UnityEngine.Random.Range(0, _screams.Count);
+            _animator.SetTrigger(_screams[randomScream].Item1);
+            yield return new WaitForSeconds(_screams[randomScream].Item2.length);
+            _isScreaming = false;
+        }
+    }
+
+    public void ScreamDev() {
+        _audioSource.PlayOneShot(_devScream);
+    }
+    
+    public void ScreamTwo() {
+        _audioSource.PlayOneShot(_scream2);
+    }
+    
+    public void ScreamThree() {
+        _audioSource.PlayOneShot(_scream3);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class GameManager : MonoBehaviour {
+    
+    bool _isPaused = false;
+    [SerializeField] GameObject _pauseMenu;
+    [SerializeField] GameObject _gameMenu;
+    [SerializeField] GameObject _startMenu;
+    
+    // A reference to two different CinemachineVirtualCameras
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera _cinemachineVirtualCamera;
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera _cinemachineVirtualCamera2;
+    
+    
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            PauseGame();
+        }
+
+    }
+
+    public void StartGame() {
+        // Fade out and disable _startMenu
+        // Transition from _cinemachineVirtualCamera to _cinemachineVirtualCamera2
+        
+        // Fade out and disable _startMenu
+        _startMenu.SetActive(false);
+        _gameMenu.SetActive(true);
+        
+        // Transition from _cinemachineVirtualCamera to _cinemachineVirtualCamera2
+        _cinemachineVirtualCamera.Priority = 0;
+        _cinemachineVirtualCamera2.Priority = 1;
         
     }
+
+    public void PauseGame() {
+        if (!_isPaused) {
+            Time.timeScale = 0;
+            _isPaused = true;
+            _pauseMenu.SetActive(true);
+        }
+        else {
+            Time.timeScale = 1;
+            _isPaused = false;
+            _pauseMenu.SetActive(false);
+        }
+    }
+    
+    public void QuitGame() {
+        Application.Quit();
+    }
+
 }
