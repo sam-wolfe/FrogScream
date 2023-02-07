c16dfca4577b2a762bb0c7ffb1d126fc37a24a55
sam-wolfe <samwolfe37@gmail.com>
2023-02-06 16:04:59 -0800

    comments

--- Assets/Scripts/FlySpawner.cs
+++ Assets/Scripts/FlySpawner.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class FlySpawner : MonoBehaviour
 {
-    // Create a public variable to hold a reference to the prefab
     public GameObject flyPrefab;
     
-    // Create a fly prefab every 10 seconds
     void Start()
     {
+    // Spawn a fly every 13 seconds
         InvokeRepeating("SpawnFly", 0, 13);
     }
     
-    // Spawn a fly at a random position
     void SpawnFly()
     {
-        // Set a random position for the fly inside the collider bounds of this GameObject
-        // Vector3 position = new Vector3(
-        //     Random.Range(transform.position.x - 1, transform.position.x + 1),
-        //     Random.Range(transform.position.y - 1, transform.position.y + 1),
-        //     transform.position.z
         // );
         
         // Instantiate the fly at the random position
